49ba6c30e5ded38cbd8077486c8ebf06b837514c
ben <b.talbot@qut.edu.au>
2019-09-05 10:05:48 +1000

    Add hello_manipulator with dummy actuation

--- src/qut_tasks/leaves/console.py
+++ src/qut_tasks/leaves/console.py
@@ -1,5 +1,10 @@
 from qut_trees.leaves import Leaf
 
+try:
+    input = raw_input
+except NameError:
+    pass
+
 
 class Print(Leaf):
 
@@ -12,3 +17,31 @@ class Print(Leaf):
     def _result_fn(self):
         print(self.loaded_data)
         return self.loaded_data
+
+
+class SelectItem(Leaf):
+    # NOTE: this is BLOCKING & should ONLY BE USED for debugging purposes
+    _DEFAULT_SELECT_TEXT = "Please select an item"
+
+    def __init__(self, select_text=None, *args, **kwargs):
+        super(SelectItem, self).__init__("Select Item",
+                                         result_fn=self._result_fn,
+                                         save=True,
+                                         *args,
+                                         **kwargs)
+        self.select_text = (SelectItem._DEFAULT_SELECT_TEXT
+                            if select_text is None else select_text)
+
+    def _result_fn(self):
+        items = self.loaded_data
+        if (not isinstance(items, list) or len(items) == 0 or
+                not any([isinstance(i, str) for i in items])):
+            ValueError(
+                "SelectItem leaf expects a list of strings as loaded_data. "
+                "It has been provided with: %s" % (items))
+
+        item = None
+        while (item is None or item not in items):
+            item = input("%s %s: " % (self.select_text, items))
+
+        return item
