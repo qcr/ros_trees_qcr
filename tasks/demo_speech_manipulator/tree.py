6560a9b32ef4b91c563d1bff9c6548e5461c5169
Steven Martin <steven.martin@qut.edu.au>
2019-10-30 14:05:34 +1000

    End to end working initial demo

--- tasks/demo_speech_manipulator/tree.py
+++ tasks/demo_speech_manipulator/tree.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from py_trees.composites import Sequence
+from py_trees.composites import Sequence, Selector
 from rv_trees.leaves import Leaf
 from rv_trees.trees import BehaviourTree
 import rv_trees.debugging as debugging
@@ -14,12 +14,13 @@ from sensor_msgs.msg import Image
 from rv_tasks.leaves.console import Print, SelectItem
 from  rv_msgs.msg import ListenGoal, ListenResult
 from  rv_msgs.srv import ParseIntentRequest, FindObjectsRequest
+from panda_speech.srv import PandaDoThingsRequest
 import rospy
 
 
 def default_intent(leaf):
-    #ret = leaf._default_load_fn()
-    ret = ParseIntentRequest(input_text='pickup the red ball') #
+    #ret = leaf._default_load_fn()   #Uncoment once debbugging done
+    ret = ParseIntentRequest(input_text='move the red ball to top right') #Comment out once debugging done
     ret.intent_type = 'panda'
     print(ret)
     return ret
@@ -38,7 +39,7 @@ get_image = SubscriberLeaf("Get Image",
                                 topic_name='/camera_driver/image_raw',
                                 topic_class=Image,
                                 save = True,
-                                expiry_time = 1.0
+                                expiry_time = 1.0 #TODO this wasn't working without exp time. Confirm fixed
                                 )
 
 #Ok lets make an inference service leaf
@@ -52,7 +53,7 @@ get_inference = ServiceLeaf("Get inference from string",
 
 def rgb_findObjects_load(leaf):
     ret = FindObjectsRequest()
-    ret.input_rgb_image = data_management.get_last_value()
+    ret.input_rgb_image = data_management.get_last_value() #TODO this isnt handled @Ben
     #print(ret)
     return ret
 
@@ -61,6 +62,26 @@ get_objects =  ServiceLeaf("Get list of objects from image",
                             service_name= '/cloudvis/yolo',
                             save = True,
                             load_fn = rgb_findObjects_load
+                            )
+
+def doThings_load(leaf):
+    #ret = leaf._default_load_fn()   #TODO this isn't handled @Ben
+    ret = PandaDoThingsRequest()
+    ret.parsed_json = data_management.get_value('intent_json').intent_json
+    #print(data_management.get_last_value().objects)
+    ret.visible_objects = data_management.get_last_value().objects #TODO Why does this need .objects but the one above for RGB doesn't
+    return ret
+
+#Call the do thing service passing list of objects and json
+do_things =  ServiceLeaf("Get instructions on what to do",
+                            service_name= '/service/panda_do_things',
+                            save = True,
+                            load_fn = doThings_load
+                            )
+
+say_string = ActionLeaf("Say some text",
+                            action_namespace='/action/say_string',
+                            save = False
                             )
 
 
@@ -73,8 +94,9 @@ def tree():
         get_inference,
         get_image,
         get_objects,
-        Print(),                #Print what was said
-                                #
+        do_things,
+        say_string,
+        Print(),                #Print what was said                                
 
         ])).run(hz=30, push_to_start=True, log_level='WARN')
 
