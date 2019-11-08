7d3ffef44a93cb0daf35b3221b143c0e727757d2
Steven Martin <steven.martin@qut.edu.au>
2019-11-08 15:16:04 +1000

    Added arm messages

--- src/rv_tasks/leaves/manipulation.py
+++ src/rv_tasks/leaves/manipulation.py
@@ -2,4 +2,25 @@ from rv_trees.leaves import Leaf
 from rv_trees.leaves_ros import (ActionLeaf, PublisherLeaf, ServiceLeaf,
                                   SubscriberLeaf)
 
+from  rv_msgs.msg import MoveToNamedPoseGoal, MoveToNamedPoseResult
+from  rv_msgs.srv import GetNamesListRequest, GetNamesListResponse
+
 # TODO
+class GetNamedGripperPoses(ServiceLeaf):
+    def __init__(self, *args, **kwargs):
+        super(GetNamedGripperPoses,
+              self).__init__("Get named gripper poses",
+                             service_name='/arm/get_named_poses',
+                             save=True,
+                             *args,
+                             **kwargs)
+
+class MoveToNamedGripperPose(ActionLeaf):
+    # TODO replace with proper action server call (& move into leaves library)
+
+    def __init__(self, *args, **kwargs):
+        super(MoveToNamedGripperPose,
+              self).__init__("Move gripper to named pose",
+                             action_namespace='/arm/cartesian/named_pose',
+                             *args,
+                             **kwargs)
