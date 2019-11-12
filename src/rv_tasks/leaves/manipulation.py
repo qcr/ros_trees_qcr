8e693e111388a166bd8a913a12fcb6cdad3bc003
acrv <panda@roboticvicision.org>
2019-11-12 14:52:12 +1000

    Finish hello_manipulator tree

--- src/rv_tasks/leaves/manipulation.py
+++ src/rv_tasks/leaves/manipulation.py
@@ -1,3 +1,6 @@
+import rv_trees.data_management as data_management
+
+
 from rv_trees.leaves import Leaf
 from rv_trees.leaves_ros import (ActionLeaf, PublisherLeaf, ServiceLeaf,
                                   SubscriberLeaf)
@@ -15,12 +18,20 @@ class GetNamedGripperPoses(ServiceLeaf):
                              *args,
                              **kwargs)
 
+
 class MoveToNamedGripperPose(ActionLeaf):
-    # TODO replace with proper action server call (& move into leaves library)
+    # TODO This should go away if the magic is setup properly
+    def _load_fn(leaf):
+        ret = MoveToNamedPoseGoal()
+        ret.pose_name = data_management.get_last_value()  
+        ret.speed = 0.2
+        return ret
 
     def __init__(self, *args, **kwargs):
         super(MoveToNamedGripperPose,
               self).__init__("Move gripper to named pose",
                              action_namespace='/arm/cartesian/named_pose',
+                             load_fn=self._load_fn,
                              *args,
                              **kwargs)
+
