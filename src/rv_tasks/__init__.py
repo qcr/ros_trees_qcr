6a13ebff14821d4c3c807bb08a713fcdf4dbd54a
Gavin Suddrey <g.suddrey@qut.edu.au>
2020-03-03 16:55:35 +1000

    Removed auto __init__ as omron breaks stuff

--- src/rv_tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import branches
-from . import leaves
-
-__all__ = ['branches', 'leaves']
--- src/rv_tasks/leaves/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import generic
-from . import manipulation
-from . import omron
-from . import vision
-
-__all__ = ['generic', 'manipulation', 'omron', 'vision']
