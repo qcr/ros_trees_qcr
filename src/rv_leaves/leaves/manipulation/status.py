a825cfd0556d4f09aa53d32a311b4f29fcd84921
suddrey <gavin.suddrey@gmail.com>
2020-09-05 20:29:46 +1000

    Added depedency for ros_numpy; removed franka specific message

--- src/rv_leaves/leaves/manipulation/status.py
+++ src/rv_leaves/leaves/manipulation/status.py
@@ -4,7 +4,7 @@ import math
 from rv_trees.leaves_ros import SubscriberLeaf, ServiceLeaf
 
 from rv_msgs.msg import ManipulatorState
-from franka_msgs.msg import FrankaState
+# from franka_msgs.msg import FrankaState
 
 class IsErrored(SubscriberLeaf):
   def __init__(self, name='Checking arm for errors', timeout=2, topic_name='/arm/state', topic_class=ManipulatorState, result_fn=None, *args, **kwargs):
@@ -47,41 +47,41 @@ class IsContacting(SubscriberLeaf):
   def result_fn(self):
     return any(self._cached_data.cartesian_contact)
 
-class IsItemGrabbed(SubscriberLeaf):
-  def __init__(self, name='item Grabbed', topic_name='/franka_state_controller/franka_states', topic_class=FrankaState, result_fn=None, *args, **kwargs):
-    super(IsItemGrabbed, self).__init__(
-      name=name,
-      topic_name=topic_name,
-      topic_class=topic_class,
-      result_fn=self.result_fn,
-      *args,
-      **kwargs
-    )
-    self.tau_J = None
-    self.ticks = 0
+# class IsItemGrabbed(SubscriberLeaf):
+#   def __init__(self, name='item Grabbed', topic_name='/franka_state_controller/franka_states', topic_class=FrankaState, result_fn=None, *args, **kwargs):
+#     super(IsItemGrabbed, self).__init__(
+#       name=name,
+#       topic_name=topic_name,
+#       topic_class=topic_class,
+#       result_fn=self.result_fn,
+#       *args,
+#       **kwargs
+#     )
+#     self.tau_J = None
+#     self.ticks = 0
 
-  def result_fn(self):
-    if not self._cached_data:
-      return None
+#   def result_fn(self):
+#     if not self._cached_data:
+#       return None
     
-    if not self.tau_J:
-      self.tau_J = self._cached_data.tau_J
-      return None
+#     if not self.tau_J:
+#       self.tau_J = self._cached_data.tau_J
+#       return None
     
-    error = self._cached_data.tau_J[2] - self.tau_J[2]
-    print('{}: {}'.format(self.ticks, error))
-    if error > 0.35:
-      self.ticks += 1
+#     error = self._cached_data.tau_J[2] - self.tau_J[2]
+#     print('{}: {}'.format(self.ticks, error))
+#     if error > 0.35:
+#       self.ticks += 1
 
-      if self.ticks >= 2:
-        self.tau_J = None
-        self.ticks = 0
-        return True
+#       if self.ticks >= 2:
+#         self.tau_J = None
+#         self.ticks = 0
+#         return True
 
-      return False
+#       return False
 
-    self.ticks = 0    
-    return False
+#     self.ticks = 0    
+#     return False
 
 class GetEEPose(SubscriberLeaf):
   def __init__(self, name='Get EE Pose', topic_name='/arm/state', topic_class=ManipulatorState, result_fn=None, *args, **kwargs):
